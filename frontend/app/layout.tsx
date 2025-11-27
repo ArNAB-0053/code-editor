6bfdde2c9f7ea841bdcd3a02b1e49abe9c6c656c
arnab-capsitech <arnab.bhattacharyya@capsitech.com>
2025-11-27 11:14:53 +0530

    testing workflow by pushing wrong code

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
 }>) {
   const theme = await getCookiesServer("theme");
   const font = await getCookiesServer("font");
-  console.log(" SERVER ---> ", theme, font);
+  console.log(" SERVER ---> ", theme, font);dsadsa
   return (
     <html lang="en" data-theme={theme} suppressHydrationWarning>
       <body
