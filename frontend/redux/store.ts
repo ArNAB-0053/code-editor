63c632dcd25254a7bfcccf32eb3d228314b8b16f
arnab-capsitech <arnab.bhattacharyya@capsitech.com>
2025-12-16 13:57:36 +0530

    feat(expiration): add expiration logic for persisted user state and implement persistedAt tracking

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,40 +1,43 @@
 import { persistReducer, persistStore } from "redux-persist";
-import preferencesReducer from "./slices/preferenceSlice"
-import activeFieldReducer from "./slices/activeFieldSlice"
+import preferencesReducer from "./slices/preferenceSlice";
+import activeFieldReducer from "./slices/activeFieldSlice";
 import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slices/userSlice"
-import editorCodeReducer from "./slices/editorSlice"
+import userReducer from "./slices/userSlice";
+import editorCodeReducer from "./slices/editorSlice";
 
 const persistPreferenceConfig = {
-    key: "prefercence",
-    storage
-}
+  key: "preference",
+  storage,
+};
 
 const persistUserConfig = {
   key: "user",
-  storage
-}
+  storage,
+};
 
 // const persistEditorConfig_temp = {
 //   key: "editor",
 //   storage
 // }
 
-const persistedPreference = persistReducer(persistPreferenceConfig, preferencesReducer)
+const persistedPreference = persistReducer(
+  persistPreferenceConfig,
+  preferencesReducer
+);
 
-const persistedUserDetails = persistReducer(persistUserConfig, userReducer)
+const persistedUserDetails = persistReducer(persistUserConfig, userReducer);
 
 // const persistedEditor_temp =  persistReducer(persistEditorConfig_temp, editorCodeReducer)
 
 export const store = configureStore({
-    reducer: {
-        preferences: persistedPreference,
-        activeField: activeFieldReducer,
-        user: persistedUserDetails,
-        editorCode: editorCodeReducer
-    },
-    middleware: (getDefaultMiddleware) =>
+  reducer: {
+    preferences: persistedPreference,
+    activeField: activeFieldReducer,
+    user: persistedUserDetails,
+    editorCode: editorCodeReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [
@@ -48,9 +51,9 @@ export const store = configureStore({
         ignoredPaths: ["register"],
       },
     }),
-})
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
